476bff9ab4f29c65483406df923fadc53f21de50
Reyes <sebasreyes866@gmail.com>
2023-10-05 19:22:55 -0600

    Se agregaron los 10 articulos mas recintes en la pagina de home

--- Downloads/PROYECTO-DW/frontend/src/components/Home.jsx
+++ Downloads/PROYECTO-DW/frontend/src/components/Home.jsx
@@ -2,20 +2,33 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-const Home = ({ articles }) => {
-  //const [data, setData] = useState([]);
-  //const [recentArticles, setRecentArticles] = useState([]); // Añadido para manejar artículos recientes
+const Home = () => {
+  const [articles, setArticles] = useState([]);
+  
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const response = await axios.get('http://localhost/Articles.php');
+        setArticles(response.data);
+      } catch (error) {
+        console.error('Error fetching articles:', error);
+      }
+    };
+
+    fetchArticles();
+  }, []);
 
   const sortedArticles = [...articles].sort((a, b) => b.id - a.id);
+  const recentArticles = sortedArticles.slice(0, 10);  
 
   return (
     <div className="container my-5">
       <div className="row justify-content-center">
         <div className="col-12 col-md-8">
-          {/* Sección de Artículos */}
+          
           <h2 className="text-center mb-4">Artículos Recientes</h2>
           <div className="list-group">
-            {sortedArticles.map((article, index) => (
+            {recentArticles.map((article, index) => (
               <Link to={`/article/${article.id}`} key={index} className="list-group-item list-group-item-action">
                 <div className="d-flex w-100 justify-content-between">
                   <h5 className="mb-1">{article.title}</h5>
