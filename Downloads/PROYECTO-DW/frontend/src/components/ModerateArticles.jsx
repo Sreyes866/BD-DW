cd628a2f772e82c0553c6ed163f63dabc3db3c8b
Reyes <sebasreyes866@gmail.com>
2023-10-24 02:18:42 -0600

    Se agregaron los articulos a la pagina de moderar, con sus respectivas plantillas y su informacion completa

--- Downloads/PROYECTO-DW/frontend/src/components/ModerateArticles.jsx
+++ Downloads/PROYECTO-DW/frontend/src/components/ModerateArticles.jsx
@@ -1,18 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Template1 from './Template1';  
+import Template2 from './Template2';
+import Template3 from './Template3';
 
 const ModerateArticles = () => {
   const [articles, setArticles] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [subcategories, setSubcategories] = useState([]);
 
-  useEffect(() => {
-    
+  const fetchData = () => {
     axios.get('http://localhost/Articles.php')
       .then(response => {
         if (Array.isArray(response.data)) {
-          setArticles(response.data.map(article => ({ 
-            id: article.id, 
-            title: article.title, 
-            status: 'Pending'  
+          setArticles(response.data.map(article => ({
+            ...article,
+            status: 'Pending'
           })));
         } else {
           console.error('Data is not an array:', response.data);
@@ -21,6 +24,18 @@ const ModerateArticles = () => {
       .catch(error => {
         console.error('Error fetching articles:', error);
       });
+
+    axios.get('http://localhost/Categories.php')
+      .then(response => setCategories(response.data))
+      .catch(error => console.error('Error fetching categories:', error));
+
+    axios.get('http://localhost/Subcategories.php')
+      .then(response => setSubcategories(response.data))
+      .catch(error => console.error('Error fetching subcategories:', error));
+  };
+
+  useEffect(() => {
+    fetchData();
   }, []);
 
   const handleApprove = (id) => {
@@ -28,7 +43,6 @@ const ModerateArticles = () => {
       article.id === id ? { ...article, status: 'Approved' } : article
     );
     setArticles(updatedArticles);
-    console.log(`Artículo ${id} aprobado`);
   };
 
   const handleReject = (id) => {
@@ -36,7 +50,19 @@ const ModerateArticles = () => {
       article.id === id ? { ...article, status: 'Rejected' } : article
     );
     setArticles(updatedArticles);
-    console.log(`Artículo ${id} rechazado`);
+  };
+
+  const ArticleRenderer = ({ article, categories, subcategories }) => {
+    switch(article.template_type) {
+      case 'Template1':
+        return <Template1 article={article} categories={categories} subcategories={subcategories} />;
+      case 'Template2':
+        return <Template2 article={article} categories={categories} subcategories={subcategories} />;
+      case 'Template3':
+        return <Template3 article={article} categories={categories} subcategories={subcategories} />;
+      default:
+        return <p>Plantilla no soportada</p>;
+    }
   };
 
   return (
@@ -45,7 +71,7 @@ const ModerateArticles = () => {
       <ul>
         {articles.map((article) => (
           <li key={article.id}>
-            {article.title} - {article.status}
+            <ArticleRenderer article={article} categories={categories} subcategories={subcategories} />
             <button onClick={() => handleApprove(article.id)}>Approve</button>
             <button onClick={() => handleReject(article.id)}>Reject</button>
           </li>
@@ -56,4 +82,8 @@ const ModerateArticles = () => {
 };
 
 export default ModerateArticles;
+
+
+
+
 
