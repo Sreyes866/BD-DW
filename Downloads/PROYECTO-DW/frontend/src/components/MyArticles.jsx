ec438d55c5b359fbb1d42fa30db5e02c5bbae7c6
Reyes <sebasreyes866@gmail.com>
2023-10-27 01:47:50 -0600

    Se configuro para que en mis articulos, aparezcan los articulos publicados, los articulos que se envian a draft y los articulos que estan en revision (Falta que sean solo los articulos del autor que esta loggueado)

--- Downloads/PROYECTO-DW/frontend/src/components/MyArticles.jsx
+++ Downloads/PROYECTO-DW/frontend/src/components/MyArticles.jsx
@@ -1,33 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const MyArticles = () => {
   const [showArticles, setShowArticles] = useState({ published: false, drafts: false, review: false });
-  const publishedArticles = ['Artículo 1', 'Artículo 2'];
-  const draftArticles = ['Draft 1', 'Draft 2'];
-  const reviewArticles = ['Revisión 1', 'Revisión 2'];
+  const [articles, setArticles] = useState([]);
 
   const toggleShowArticles = (type) => {
     setShowArticles({ ...showArticles, [type]: !showArticles[type] });
   };
+
+  useEffect(() => {
+    axios.get('http://localhost/Articles.php')
+      .then(response => {
+        setArticles(response.data);
+      })
+      .catch(error => console.error('Error fetching articles:', error));
+  }, []);
+
+  const publishedArticles = articles.filter(article => article.approval_status === 'Approved');
+  const draftArticles = articles.filter(article => article.publish_status === 'Draft');  
+  const reviewArticles = articles.filter(article => article.approval_status === 'Pending');
 
   return (
     <div className="my-articles-container">
       <div className="article-section">
         <h3 onClick={() => toggleShowArticles('published')}>Artículos Publicados</h3>
         <div className={`article-list ${showArticles.published ? 'show' : ''}`}>
-          {publishedArticles.map(article => <div key={article}>{article}</div>)}
+          {publishedArticles.map(article => <div key={article.id}>{article.title}</div>)}
         </div>
       </div>
       <div className="article-section">
         <h3 onClick={() => toggleShowArticles('drafts')}>Artículos sin Publicar (Drafts)</h3>
         <div className={`article-list ${showArticles.drafts ? 'show' : ''}`}>
-          {draftArticles.map(article => <div key={article}>{article}</div>)}
+          {draftArticles.map(article => <div key={article.id}>{article.title}</div>)}
         </div>
       </div>
       <div className="article-section">
         <h3 onClick={() => toggleShowArticles('review')}>Artículos en Revisión</h3>
         <div className={`article-list ${showArticles.review ? 'show' : ''}`}>
-          {reviewArticles.map(article => <div key={article}>{article}</div>)}
+          {reviewArticles.map(article => <div key={article.id}>{article.title}</div>)}
         </div>
       </div>
     </div>
@@ -35,3 +46,4 @@ const MyArticles = () => {
 };
 
 export default MyArticles;
+
