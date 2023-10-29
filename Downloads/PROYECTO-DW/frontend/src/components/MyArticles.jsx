6167e3417f7d325c8497bc14817e9dc7818b784d
Reyes <sebasreyes866@gmail.com>
2023-10-28 19:10:01 -0600

    Se agrego que se puedan modificar los articulos en mis articulos y que los articulos que estan en draft no aparezcan en revision

--- Downloads/PROYECTO-DW/frontend/src/components/MyArticles.jsx
+++ Downloads/PROYECTO-DW/frontend/src/components/MyArticles.jsx
@@ -6,71 +6,133 @@ import Template2 from './Template2';
 import Template3 from './Template3';
 
 const MyArticles = () => {
-  const [showArticles, setShowArticles] = useState({ published: false, drafts: false, review: false });
   const [articles, setArticles] = useState([]);
+  const [editingArticle, setEditingArticle] = useState(null);
   const { userName } = useAuth();
+  const [categories, setCategories] = useState([]);
+  const [subcategories, setSubcategories] = useState([]);
 
-  useEffect(() => {
+
+  const fetchData = () => {
     axios.get('http://localhost/Articles.php')
       .then(response => {
         const myArticles = response.data.filter(article => article.author_id === userName);
         setArticles(myArticles);
       })
-      .catch(error => console.error('Error:', error));
+      .catch(error => console.error('Error fetching articles:', error));
+  };
+  
+  useEffect(() => {
+    fetchData();  
+  
+    
+    axios.get('http://localhost/Categories.php')
+      .then(response => setCategories(response.data))
+      .catch(error => console.error('Error fetching categories:', error));
+  
+    
+    axios.get('http://localhost/Subcategories.php')
+      .then(response => setSubcategories(response.data))
+      .catch(error => console.error('Error fetching subcategories:', error));
   }, [userName]);
+  
 
-  const toggleShowArticles = (type) => {
-    setShowArticles({ ...showArticles, [type]: !showArticles[type] });
+  const handleDelete = (id) => {
+    axios.post('http://localhost/deleteArticle.php', { id: id })
+      .then(response => {
+        fetchData();
+      })
+      .catch(error => console.error('Error deleting article:', error));
+  };
+
+
+
+  const handleEdit = (article) => {
+    setEditingArticle(article);
+  };
+
+  const handleSave = () => {
+    axios.post('http://localhost/updateArticle.php', editingArticle)
+      .then(response => {
+        setEditingArticle(null);
+        fetchData();
+      })
+      .catch(error => console.error('Error updating article:', error));
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setEditingArticle({ ...editingArticle, [name]: value });
   };
 
   const publishedArticles = articles.filter(article => article.approval_status === 'Approved');
   const draftArticles = articles.filter(article => article.publish_status === 'Draft');
-  const reviewArticles = articles.filter(article => article.approval_status === 'Pending');
+  const reviewArticles = articles.filter(
+    article => article.approval_status === 'Pending' && article.publish_status !== 'Draft'
+  );
 
   return (
-    <div className="my-articles-container">
+    <div className="container">
       <div className="article-section">
-        <h3 onClick={() => toggleShowArticles('published')}>Artículos Publicados</h3>
-        <div className={`article-list ${showArticles.published ? 'show' : ''}`}>
-          {publishedArticles.map(article => (
-            <ArticleRenderer key={article.id} article={article} />
-          ))}
-        </div>
+        <h3>Artículos Publicados</h3>
+        {publishedArticles.map((article, index) => (
+          <ArticleRenderer key={index} article={article} categories={categories} subcategories={subcategories} isEditing={editingArticle?.id === article.id} handleChange={handleInputChange} handleEdit={handleEdit} handleSave={handleSave} handleDelete={handleDelete} />
+        ))}
       </div>
+
       <div className="article-section">
-        <h3 onClick={() => toggleShowArticles('drafts')}>Artículos sin Publicar (Drafts)</h3>
-        <div className={`article-list ${showArticles.drafts ? 'show' : ''}`}>
-          {draftArticles.map(article => (
-            <ArticleRenderer key={article.id} article={article} />
-          ))}
-        </div>
+        <h3>Artículos sin Publicar (Drafts)</h3>
+        {draftArticles.map((article, index) => (
+          <ArticleRenderer key={index} article={article} categories={categories} subcategories={subcategories} isEditing={editingArticle?.id === article.id} handleChange={handleInputChange} handleEdit={handleEdit} handleSave={handleSave} handleDelete={handleDelete} />
+        ))}
       </div>
+
       <div className="article-section">
-        <h3 onClick={() => toggleShowArticles('review')}>Artículos en Revisión</h3>
-        <div className={`article-list ${showArticles.review ? 'show' : ''}`}>
-          {reviewArticles.map(article => (
-            <ArticleRenderer key={article.id} article={article} />
-          ))}
-        </div>
+        <h3>Artículos en Revisión</h3>
+        {reviewArticles.map((article, index) => (
+          <ArticleRenderer key={index} article={article} categories={categories} subcategories={subcategories} isEditing={editingArticle?.id === article.id} handleChange={handleInputChange} handleEdit={handleEdit} handleSave={handleSave} handleDelete={handleDelete} />
+        ))}
       </div>
     </div>
   );
 };
 
-const ArticleRenderer = ({ article }) => {
+const renderTemplate = (article, isEditing, handleChange, categories, subcategories) => {
   switch(article.template_type) {
     case 'Template1':
-      return <Template1 article={article} />;
+      return <Template1 article={article} isEditing={isEditing} handleChange={handleChange} categories={categories} subcategories={subcategories} />;
     case 'Template2':
-      return <Template2 article={article} />;
+      return <Template2 article={article} isEditing={isEditing} handleChange={handleChange} categories={categories} subcategories={subcategories} />;
     case 'Template3':
-      return <Template3 article={article} />;
+      return <Template3 article={article} isEditing={isEditing} handleChange={handleChange} categories={categories} subcategories={subcategories} />;
     default:
       return <p>Plantilla no soportada</p>;
   }
 };
 
+const ArticleRenderer = ({ article, categories, subcategories, isEditing, handleChange, handleEdit, handleSave, handleDelete }) => {
+  return (
+    <div style={{ border: '1px solid #ccc', margin: '20px', padding: '15px' }}>
+      {isEditing ? (
+        <>
+          {renderTemplate(article, isEditing, handleChange, categories, subcategories)}
+          <button onClick={handleSave}>Guardar</button>
+          <button onClick={() => handleEdit(null)}>Cancelar</button>
+        </>
+      ) : (
+        <>
+          {renderTemplate(article, false, null, categories, subcategories)}
+          <button onClick={() => handleEdit(article)}>Editar</button>
+          <button onClick={() => handleDelete(article.id)}>Eliminar</button>
+        </>
+      )}
+    </div>
+  );
+};
+
+
 export default MyArticles;
+
 
 
 
